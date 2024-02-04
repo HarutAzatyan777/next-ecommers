600df4c8d38fdefc2d353e72bad9f82a792c8ba8
HarutAzatyan777 <harutazatyan45@gmail.com>
2024-02-04 23:48:35 +0400

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+// pages/_app.js
+
+import { SessionProvider } from "next-auth/react";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
+
+export default App;
