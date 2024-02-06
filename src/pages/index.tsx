a667b36211c19eae682759fdcd58187f61598468
sky <harutazatyan45@gmail.com>
2024-02-06 15:39:07 +0400

    06.02.2024-4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "../../components/nav"
 
 
 export default function Home() {
@@ -15,6 +16,11 @@ export default function Home() {
 }
 
 return(
-  <div>logged in{session.user?.email}</div>
+  <div className="bg-blue-900 min-h-screen flex">
+    <Nav />
+    <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
+    logged in {session.user?.email}
+    </div>
+  </div>
 );
 }
